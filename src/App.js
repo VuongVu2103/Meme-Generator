9dadf369809f4e18103ec281ed238b27c2fd2945
VuongVu <hoangvu2103@gmail.com>
2021-06-14 15:26:59 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MemeGenerator from './components/MemeGenerator'
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
